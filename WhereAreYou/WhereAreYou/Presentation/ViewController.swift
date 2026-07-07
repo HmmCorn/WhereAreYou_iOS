e383ea76f9a26eda9d1b1364d629ebfcd9227e34
Sang Yu Lee <sangyu857@gmail.com>
2026-07-07 18:15:23 +0900

    CHORE: 프로젝트 생성

---
+++ WhereAreYou/WhereAreYou/Presentation/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-07-07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
+
