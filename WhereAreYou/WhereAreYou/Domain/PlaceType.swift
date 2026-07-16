8a3830dda4282b7cc5df065104f4a1b06e4d0975
Sang Yu Lee <sangyu857@gmail.com>
2026-07-16 23:14:32 +0900

    REFACTOR: UI에 필요한 PlaceType의 속성 계층 이동

--- WhereAreYou/WhereAreYou/Domain/PlaceType.swift
+++ WhereAreYou/WhereAreYou/Domain/PlaceType.swift
@@ -5,7 +5,7 @@
 //  Created by 이상유 on 2026-07-14.
 //
 
-enum PlaceType {
+enum PlaceType: CaseIterable, Hashable {
 
     case subway
     case restaurant
@@ -14,17 +14,5 @@ enum PlaceType {
     case station
     case shop
     case other
-
-    var title: String {
-        switch self {
-        case .subway: return "지하철역"
-        case .restaurant: return "레스토랑"
-        case .cafe: return "카페"
-        case .hospital: return "병원"
-        case .station: return "정류장"
-        case .shop: return "쇼핑"
-        case .other: return "기타"
-        }
-    }
     
 }
---
+++ WhereAreYou/WhereAreYou/Presentation/Extension/PlaceType+.swift
@@ -0,0 +1,36 @@
+//
+//  PlaceType+.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-07-16.
+//
+
+import UIKit
+
+extension PlaceType {
+
+    var title: String {
+        switch self {
+        case .subway: return "지하철역"
+        case .restaurant: return "레스토랑"
+        case .cafe: return "카페"
+        case .hospital: return "병원"
+        case .station: return "정류장"
+        case .shop: return "쇼핑"
+        case .other: return "기타"
+        }
+    }
+
+    var color: UIColor {
+        switch self {
+        case .subway: return .systemGreen
+        case .restaurant: return .systemYellow
+        case .cafe: return .orange
+        case .hospital: return .systemIndigo
+        case .station: return .systemBlue
+        case .shop: return .systemRed
+        case .other: return .systemBrown
+        }
+    }
+
+}
