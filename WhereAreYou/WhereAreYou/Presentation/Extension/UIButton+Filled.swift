133abdabeed63d350afc86c20da5592a2ae5e0f9
Sang Yu Lee <sangyu857@gmail.com>
2026-07-17 00:28:53 +0900

    REFACTOR: UIButton+Filled의 활용도 수정

--- WhereAreYou/WhereAreYou/Presentation/Extension/UIButton+Filled.swift
+++ WhereAreYou/WhereAreYou/Presentation/Extension/UIButton+Filled.swift
@@ -10,13 +10,29 @@ import UIKit
 // MARK: - 꽉 찬 스타일의 버튼
 
 extension UIButton {
-    static func filled(title: String, background: UIColor?, tint: UIColor) -> UIButton {
+    static func filled(
+        title: String,
+        background: UIColor?,
+        tint: UIColor,
+        font: UIFont.TextStyle? = nil,
+        edgeInsets: NSDirectionalEdgeInsets = .init(top: 8, leading: 0, bottom: 8, trailing: 0)
+    ) -> UIButton {
+        
         var config = UIButton.Configuration.filled()
-        config.title = title
+        if let font {
+            var attributedTitle = AttributedString(title)
+            attributedTitle.font = .preferredFont(forTextStyle: font)
+            config.attributedTitle = attributedTitle
+        } else {
+            config.title = title
+        }
         config.baseBackgroundColor = background
         config.baseForegroundColor = tint
-        config.cornerStyle = .large
-        config.contentInsets = NSDirectionalEdgeInsets(top: 8, leading: 0, bottom: 8, trailing: 0)
-        return UIButton(configuration: config)
+        config.contentInsets = edgeInsets
+
+        let button = UIButton(configuration: config)
+        button.layer.cornerRadius = 12
+
+        return button
     }
 }
