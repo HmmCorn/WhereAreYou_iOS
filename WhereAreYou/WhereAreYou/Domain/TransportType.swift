434004959f2b878e4584985f7f2519e3c3e18a9f
Sang Yu Lee <sangyu857@gmail.com>
2026-07-16 16:31:16 +0900

    REFACTOR: UI에만 필요한 TransportType의 속성 extension으로 이동

--- WhereAreYou/WhereAreYou/Domain/TransportType.swift
+++ WhereAreYou/WhereAreYou/Domain/TransportType.swift
@@ -12,22 +12,4 @@ enum TransportType {
     case transit
     case bicycle
 
-    var name: String {
-        switch self {
-        case .walk: return "걷기"
-        case .car: return "자동차"
-        case .transit: return "대중교통"
-        case .bicycle: return "자전거"
-        }
-    }
-
-    var icon: String {
-        switch self {
-        case .walk: return "figure.walk"
-        case .car: return "car"
-        case .transit: return "bus"
-        case .bicycle: return "bicycle"
-        }
-    }
-
 }
---
+++ WhereAreYou/WhereAreYou/Presentation/Extension/TransportType+.swift
@@ -0,0 +1,39 @@
+//
+//  TransportType+.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-07-14.
+//
+
+import UIKit
+
+extension TransportType {
+
+    var name: String {
+        switch self {
+        case .walk: return "걷기"
+        case .car: return "자동차"
+        case .transit: return "대중교통"
+        case .bicycle: return "자전거"
+        }
+    }
+
+    var icon: String {
+        switch self {
+        case .walk: return "figure.walk"
+        case .car: return "car"
+        case .transit: return "bus"
+        case .bicycle: return "bicycle"
+        }
+    }
+
+    var color: UIColor {
+        switch self {
+        case .walk: return .systemBrown
+        case .car: return .systemIndigo
+        case .transit: return .systemGreen
+        case .bicycle: return .systemYellow
+        }
+    }
+
+}
