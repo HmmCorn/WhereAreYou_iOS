eb93422a72939984bfaaad1ece5ca45879e0c7e2
snughnu <ksnughnu@gmail.com>
2026-07-16 18:41:05 +0900

    FEAT: 날짜/시간 및 남은 시간 표시 포맷 추가

--- WhereAreYou/WhereAreYou/Core/Date+Format.swift
+++ WhereAreYou/WhereAreYou/Core/Date+Format.swift
@@ -9,6 +9,23 @@ import Foundation
 
 extension Date {
 
+    var koreanDateString: String {
+        Self.koreanDateFormatter.string(from: self)
+    }
+
+    var koreanDateTimeString: String {
+        Self.koreanDateTimeFormatter.string(from: self)
+    }
+
+    var remainingTimeText: String {
+        let hours = Calendar.current.dateComponents([.hour], from: Date(), to: self).hour ?? 0
+
+        if hours >= 24 {
+            return "\(hours / 24)일 남음"
+        }
+        return "\(hours)시간 남음"
+    }
+
     private static let koreanDateFormatter: DateFormatter = {
         let formatter = DateFormatter()
         formatter.locale = Locale(identifier: "ko_KR")
@@ -16,8 +33,11 @@ extension Date {
         return formatter
     }()
 
-    var koreanDateString: String {
-        Self.koreanDateFormatter.string(from: self)
-    }
+    private static let koreanDateTimeFormatter: DateFormatter = {
+        let formatter = DateFormatter()
+        formatter.locale = Locale(identifier: "ko_KR")
+        formatter.dateFormat = "yyyy년 MM월 dd일 HH:mm"
+        return formatter
+    }()
 
 }
