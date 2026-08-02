90bcf64cf8d3b717ebd96296613b8ca8347c0e0b
Sang Yu Lee <sangyu857@gmail.com>
2026-08-03 01:08:52 +0900

    FEAT: 채팅 presentation model

---
+++ WhereAreYou/WhereAreYou/Presentation/PresentationModel/ChatBubbleItem.swift
@@ -0,0 +1,19 @@
+//
+//  ChatBubbleItem.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-08-03.
+//
+
+import Foundation
+
+/// 채팅 기본 정보
+struct ChatBubbleItem {
+    let id: String
+    let senderID: String
+    let senderNickname: String
+    let senderProfileImage: String
+    let content: String
+    let timeText: String?
+    let sentAt: Date
+}
