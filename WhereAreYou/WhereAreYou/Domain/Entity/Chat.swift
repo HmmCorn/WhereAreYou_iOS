630966f5bc1ba5cd8691f63e7721b77a5c1dc660
Sang Yu Lee <sangyu857@gmail.com>
2026-08-03 01:08:23 +0900

    FEAT: Chat 엔티티

---
+++ WhereAreYou/WhereAreYou/Domain/Entity/Chat.swift
@@ -0,0 +1,16 @@
+//
+//  Chat.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-07-30.
+//
+
+import Foundation
+
+struct Chat {
+    let id: String
+    let appointmentID: String
+    let sender: User
+    let text: String
+    let sentAt: Date
+}
