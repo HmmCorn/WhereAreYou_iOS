8a37a31a61f87d17394d1d5a11f71757c8d66c59
Sang Yu Lee <sangyu857@gmail.com>
2026-08-03 03:07:26 +0900

    FEAT: 공유된 장소 presentation model

---
+++ WhereAreYou/WhereAreYou/Presentation/PresentationModel/SharedPlaceItem.swift
@@ -0,0 +1,18 @@
+//
+//  SharedPlaceItem.swift
+//  WhereAreYou
+//
+//  Created by 이상유 on 2026-08-03.
+//
+
+import Foundation
+
+struct SharedPlaceItem {
+    let id: String
+    let placeName: String
+    let placeAddress: String
+    let voterProfileImages: [String]
+    let voterCount: Int
+    let hasVoted: Bool
+    let sharedAt: Date
+}
