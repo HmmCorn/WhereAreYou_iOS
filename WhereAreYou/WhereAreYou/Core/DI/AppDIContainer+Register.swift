36d9a5529a6ba5fe7ad0f0b0ae528d5339a3858a
snughnu <ksnughnu@gmail.com>
2026-08-26 20:19:00 +0900

    FEAT: DIContainer 코어 및 의존성 등록부 추가

---
+++ WhereAreYou/WhereAreYou/Core/DI/AppDIContainer+Register.swift
@@ -0,0 +1,34 @@
+//
+//  AppDIContainer+Register.swift
+//  WhereAreYou
+//
+//  Created by 김성훈 on 8/26/26.
+//
+
+extension DIContainer {
+
+    func registerDependencies() {
+        // 위치
+        let coreLocationRepository = CoreLocationRepository()
+        register(LocationRepository.self, instance: coreLocationRepository)
+        register(LocationPermissionRepository.self, instance: coreLocationRepository)
+
+        // 약속 생성/조회
+        register(AppointmentCreationRepository.self) { MockAppointmentCreationRepository() }
+        register(AppointmentDetailRepository.self) { MockAppointmentDetailRepository() }
+        register(AppointmentInfoRepository.self) { MockAppointmentInfoRepository() }
+
+        // 채팅
+        register(ChatRepository.self) { MockChatRepository() }
+
+        // 장소 검색/조회
+        register(NearbyPlaceRepository.self) { MockNearbyPlaceRepository() }
+        register(PlaceSearchRepository.self) { MockPlaceSearchRepository() }
+        register(ReverseGeocodingRepository.self) { MockReverseGeocodingRepository() }
+        register(SharedPlaceRepository.self) { MockSharedPlaceRepository() }
+
+        // 길찾기
+        register(RouteSearchRepository.self) { MockRouteSearchRepository() }
+    }
+
+}
