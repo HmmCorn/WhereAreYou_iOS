497a3832ddcdc378c8c27b9730da1ab9f843b01f
snughnu <ksnughnu@gmail.com>
2026-09-03 14:14:28 +0900

    FIX: SceneDelegate의 window 강제 언래핑 제거

--- WhereAreYou/WhereAreYou/Core/SceneDelegate.swift
+++ WhereAreYou/WhereAreYou/Core/SceneDelegate.swift
@@ -14,9 +14,10 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
-        window = UIWindow(windowScene: windowScene)
+        let window = UIWindow(windowScene: windowScene)
+        self.window = window
 
-        let coordinator = AppCoordinator(window: window!)
+        let coordinator = AppCoordinator(window: window)
         appCoordinator = coordinator
         coordinator.start()
     }
